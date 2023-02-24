485507b9c68d26d4df68c405bf7fd772c056086a
ExplodingBottle <72459125+ExplodingBottle@users.noreply.github.com>
2023-02-24 16:58:00 +0100

    Removed GPLv3

--- v1/scripts/agent_update.js
+++ v1/scripts/agent_update.js
@@ -1,21 +1,3 @@
-/*
- *   ExplodingAU Website - The website for ExplodingAU
- *   Copyright (C) 2023  ExplodingBottle
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 const endpoint = "https://explodingbottle.github.io/eaudl/v1";
 
 const agentDl = "/agent/agentlatest.ln";
